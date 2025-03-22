104fd16a34201ab84656e5a24ab361e9f12b136f
Unju Shizen Nouen <unjushizennouen@gmail.com>
2025-03-22 20:54:40 +0900

    Update CropList.js for deployment

--- src/components/CropList.js
+++ src/components/CropList.js
@@ -9,7 +9,7 @@ const CropList = () => {
   useEffect(() => {
     async function fetchCrops() {
       try {
-        const response = await axios.get("https://cropdatabackend-production.up.railway.app");
+        const response = await axios.get("https://cropdatabackend-production.up.railway.app/crops");
         setCrops(response.data);
       } catch (error) {
         console.error("データ取得エラー:", error);
